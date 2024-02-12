290d390f506e62a33ea1ca74162e9d7ba70e9392
franzinBr <alan.franzin2011@gmail.com>
2024-02-12 12:57:27 -0400

    Create Api Server

---
+++ api/api.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/franzinBr/feedks-api/api/middlewares"
+	"github.com/franzinBr/feedks-api/api/routers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitServer() {
+	r := gin.Default()
+
+	registerRoutes(r)
+
+	err := r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	if err != nil {
+		log.Fatal("Error on start API")
+	}
+}
+
+func registerRoutes(r *gin.Engine) {
+	api := r.Group("/api")
+
+	v1 := api.Group("/v1")
+	{
+		health := v1.Group("/health")
+		user := v1.Group("/user")
+		feedback := v1.Group("/feedback", middlewares.Authentication())
+
+		routers.Health(health)
+		routers.User(user)
+		routers.FeedBack(feedback)
+	}
+}
